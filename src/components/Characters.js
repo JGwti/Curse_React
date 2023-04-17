c07c19946c2741aa29c48de7615e7e18425d99aa
DevelopGroup <Jgutierrezl@jdc.edu.com>
2023-04-16 21:35:43 -0500

    first commit save repository

---
+++ src/components/Characters.js
@@ -0,0 +1,51 @@
+export default function Character(props) {
+    const { characters, setCharacter } = props;
+
+    const resetCharacters = () => {
+        setCharacter(null);
+    }
+    
+    return (
+        <div className="characters">
+            <h1>Personajes</h1>
+            <samp className="back-home" onClick={resetCharacters}>Volver a la home</samp>
+            <div className="container-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}>
+                        <div>
+                            <img src={character.image} alt={character.name}></img>
+                        </div>
+                        <div>
+                            <h3>
+                                {character.name}
+                            </h3>
+                            <h6>
+                                {character.status === "Alive" ? (
+                                    <>
+                                        <samp className="alive"/>
+                                        Alive
+                                    </>
+                                ) : (
+                                    <>
+                                        <samp className="dead"/>
+                                        Dead
+                                    </>
+                                )}
+                            </h6>
+                            <p>
+                                <span className="text-grey">Episodios: </span>
+                                <span>{ character.episode.length }</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Especie: </span>
+                                <span>{ character.species }</span>
+                            </p>
+                        </div>
+                    </div>
+                ) // return implicito
+                )}
+            </div>
+            <span className="back-home" onClick={resetCharacters}>Volver a la home</span>
+        </div>
+    );
+}
